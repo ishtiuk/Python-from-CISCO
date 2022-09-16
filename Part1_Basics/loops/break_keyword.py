7142ab24aab4fbc7d0038917ddae13416610f21b
Md. Ishtiuk Ahammed <93067876+ishtiuk@users.noreply.github.com>
2022-09-16 14:45:21 +0600

    Create break_keyword.py

---
+++ Part1_Basics/loops/break_keyword.py
@@ -0,0 +1,10 @@
+
+
+while True:
+    usr = input("Enter input: ")
+    
+    if usr == "chupacabra":
+        print("You've successfully left the loop.")
+        break
+
+    print("Hah hah, you've stucked in my time loop, sorry code loop. :D")

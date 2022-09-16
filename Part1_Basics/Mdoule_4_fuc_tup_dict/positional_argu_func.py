580fd5d7da6f2647ec1ec524f88d586bf9131a9f
Md. Ishtiuk Ahammed <93067876+ishtiuk@users.noreply.github.com>
2022-09-16 23:35:50 +0600

    Create positional_argu_func.py

---
+++ Part1_Basics/Mdoule_4_fuc_tup_dict/positional_argu_func.py
@@ -0,0 +1,19 @@
+
+def address(street, city, postal_code):
+    print("Your address is:", street, "St.,", city, postal_code)
+
+s = input("Street: ")
+p_c = input("Postal Code: ")
+c = input("City: ")
+
+address(s, c, p_c)
+
+
+
+def name(first_name, last_name="Smith"):
+    print(first_name, last_name)
+
+name("Andy")    # outputs: Andy Smith
+name("Betty", "Johnson")    # outputs: Betty Johnson (the keyword argument replaced by "Johnson")
+
+

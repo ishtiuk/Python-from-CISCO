71780d8ca41fdd01b7dfcbb51faf4607ec45eb48
Md. Ishtiuk Ahammed <93067876+ishtiuk@users.noreply.github.com>
2022-12-02 23:44:26 +0600

    Create ord__and__chr.py

---
+++ Part2_Intermediate_Python/STR_LST_etc_Methods/ord__and__chr.py
@@ -0,0 +1,21 @@
+
+
+# 'ord()' gives the ASCII code of particular character respectively. 
+# 'chr()' reverses that.  It gives character from ASCII code ;) 
+
+# Demonstrating the ord() function.
+
+char_1 = 'a'
+char_2 = ' '  # space
+alpha = 'α'
+
+print(ord(char_1))
+print(ord(char_2))
+
+print(ord(alpha))
+
+
+# chr() function for reverse
+
+print(chr(32))
+print(chr(97))

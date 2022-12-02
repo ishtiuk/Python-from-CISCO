43115dd5693b8bedea8a2d235c77353e82bce484
Md. Ishtiuk Ahammed <93067876+ishtiuk@users.noreply.github.com>
2022-12-02 23:21:27 +0600

    Add files via upload

---
+++ Part2_Intermediate_Python/OOP/c_first_class_and_object.py
@@ -0,0 +1,8 @@
+
+class TheSimplestClass:
+    pass
+
+
+my_first_object = TheSimplestClass()
+
+# Acctually not first for me now...! 😉, Cause I've already read these things from [Codewithharry and Programming Hero..😎
